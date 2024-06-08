cc11dfb476849271c5afc2b46212d4ed5e1769c5
Muhammad Bilal Ellahi <67937279+MuhammadBilalEllahi@users.noreply.github.com>
2024-06-09 02:01:29 +0500

    Login And Sign Up Page with bg fixed. Css in Tailwind and Daisy UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
+import Login from './pages/login/Login'
+import SignUp from './pages/signup/SignUp'
+
 
 function App() {
-  
+
 
   return (
-   <>
-   <button className="btn btn-secondary">Secondary</button>
+    <div className="p-4 h-screen flex items-center justify-center">
 
-   </>
+      {/* <Login /> */}
+      <SignUp />
+    </div>
   )
 }
 
