4db8e552a18a35bedea8937f55edf9c674a85c5d
Muhammad Bilal Ellahi <67937279+MuhammadBilalEllahi@users.noreply.github.com>
2024-06-10 01:31:30 +0500

    Auth Context  , useSignUp and useLogout

--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,9 +1,14 @@
 
 import { BiLogOut } from 'react-icons/bi'
+import useLogout from '../../hooks/useLogout'
 export default function LogoutButton() {
+    const { loading, logout } = useLogout()
     return (
         <div className='mt-auto'>
-            <BiLogOut className="w-5 h-5 text-white cursor-pointer" />
+            {!loading ? <BiLogOut onClick={logout} className="w-5 h-5 text-white cursor-pointer" /> :
+
+                <span className='loading loading-spinner '></span>}
+
         </div>
     )
 }
--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,8 +1,11 @@
 import { useState } from "react"
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 
 const useSignup = () => {
     const [loading, setLoading] = useState(false);
+
+    const { setAuthUser } = useAuthContext()
 
     const signup = async ({
         fullName, username, password, confirmPassword, gender,
@@ -22,7 +25,15 @@ const useSignup = () => {
             })
 
             const data = await res.json()
+            if (data.error) {
+                throw new Error(data.error)
+            }
             console.log(data)
+            localStorage.setItem("chat-user", JSON.stringify(data))
+            setAuthUser(data)
+
+
+
         } catch (error) {
             toast.error(error.message)
         }
