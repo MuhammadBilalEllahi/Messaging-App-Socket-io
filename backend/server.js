5948bc68f9279721392ca3f15f5fa8112525f5cd
Muhammad Bilal Ellahi <67937279+MuhammadBilalEllahi@users.noreply.github.com>
2024-06-10 20:10:40 +0500

    predeployment setup

--- backend/server.js
+++ backend/server.js
@@ -9,13 +9,13 @@ import userRoutes from './routes/user.routes.js';
 import connectToMongoDB from "./db/connect.mongodb.js";
 import { app, server } from "./socket/socket.js";
 
-
+import path from 'path'
 
 dotenv.config()
 // const server = express();
 const PORT = process.env.PORT || 9090;
 
-
+const __dirname = path.resolve()
 
 app.use(express.json())
 app.use(cookieParser())
@@ -24,9 +24,11 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messagesRoutes)
 app.use("/api/users", userRoutes)
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
 
-
-
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 
 server.listen(PORT, () => {
