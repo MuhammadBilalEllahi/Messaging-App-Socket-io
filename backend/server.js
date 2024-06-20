719d88654fde294fac705757eefd283084439397
Muhammad Bilal Ellahi <67937279+MuhammadBilalEllahi@users.noreply.github.com>
2024-06-20 18:40:35 +0500

    refactor

--- backend/server.js
+++ backend/server.js
@@ -31,21 +31,8 @@ app.use(cors(corsOptions))
 app.use(express.json())
 app.use(cookieParser())
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://messaging-app-socket-io-frontend.vercel.app");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-});
-
 // app.options('*', cors(corsOptions));
-app.options('*', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "https://messaging-app-socket-io-frontend.vercel.app");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.sendStatus(200);
-});
+app.options('*', cors());
 
 app.get("/", async (req, res) => {
     res.send("OK")
