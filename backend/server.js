813b86e6ed8a2368ecd18e8447934bc7d8372a63
Muhammad Bilal Ellahi <67937279+MuhammadBilalEllahi@users.noreply.github.com>
2024-06-30 04:00:27 +0500

    test

--- backend/server.js
+++ backend/server.js
@@ -22,23 +22,7 @@ const __dirname = path.resolve()
 
 
 
-app.use(cors({
-    origin: 'https://messaging-app-socket-io-frontend.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true
 
-}))
-app.use(express.json())
-app.use(cookieParser())
-
-// app.options('*', cors(corsOptions));
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://messaging-app-socket-io-frontend.vercel.app");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
-
-app.options('*', cors());
 // app.options('*', (req, res) => {
 //     res.header('Access-Control-Allow-Origin', 'https://messaging-app-socket-io-frontend.vercel.app');
 //     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -4,19 +4,26 @@ import express from "express"
 import cors from "cors"
 
 const app = express()
-// app.use(cors({
-//     origin: 'https://messaging-app-socket-io-frontend.vercel.app',
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     credentials: true
 
-// }))
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "https://messaging-app-socket-io-frontend.vercel.app");
-//     res.header("Access-Control-Allow-Credentials", "true");
-//     next();
-// });
+app.use(cors({
+    origin: 'https://messaging-app-socket-io-frontend.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
 
-// app.options('*', cors());
+}))
+app.use(express.json())
+app.use(cookieParser())
+
+// app.options('*', cors(corsOptions));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://messaging-app-socket-io-frontend.vercel.app");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
+app.options('*', cors());
+
+
 const server = http.createServer(app)
 const io = new Server(server, {
     // cors: {
