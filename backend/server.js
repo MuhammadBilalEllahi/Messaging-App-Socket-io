7570ac47ef161d2e425f78bafbdb2445e02befc0
Muhammad Bilal Ellahi <67937279+MuhammadBilalEllahi@users.noreply.github.com>
2024-07-01 02:21:52 +0500

    options asteric

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
     next();
 });
 
-// app.options('*', cors());
+app.options('*', cors());
 
 
 // app.options('*', (req, res) => {
@@ -49,13 +49,13 @@ app.use((req, res, next) => {
 //     res.sendStatus(200);
 // });
 
-app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Origin', 'https://messaging-app-socket-io-frontend.vercel.app');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.sendStatus(200);
-});
+// app.options('*', (req, res) => {
+//     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//     res.header('Access-Control-Allow-Origin', 'https://messaging-app-socket-io-frontend.vercel.app');
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     res.sendStatus(200);
+// });
 
 
 app.get("/", async (req, res) => {
